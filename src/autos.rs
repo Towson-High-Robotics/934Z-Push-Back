95d8977bf591f29b46ce87f268acdab7d12764aa
Blueblob860 <50087969+Blueblob860@users.noreply.github.com>
2025-10-18 20:21:12 -0400

    feat: :sparkles: Structure for Autos
    this is horribly done and shouldn't exist please rewrite like half this code ;-;

--- src/autos.rs
+++ src/autos.rs
@@ -1,6 +1,8 @@
+use alloc::vec::Vec;
+
 #[allow(dead_code)]
 #[derive(Debug, Default, Clone, Copy, Eq, PartialEq)]
-pub(crate) enum Autos {
+pub(crate) enum AutoTypes {
     RedLeft,
     RedRight,
     RedAWP,
@@ -38,4 +40,111 @@ impl PID {
         self.last_err = 0.0;
         self.sum_err = 0.0;
     }
+}
+
+pub(crate) struct SpeedCurve {
+    start_speed: f64,
+    control_1: f64,
+    end_speed: f64,
+}
+
+impl SpeedCurve {
+    pub fn new_linear(start: f64, end: f64) -> Self {
+        Self {
+            start_speed: start,
+            control_1: (start + end) / 2.0,
+            end_speed: end,
+        }
+    }
+
+    pub fn new(start_speed: f64, control_1: f64, end_speed: f64) -> Self { Self { start_speed, control_1, end_speed } }
+
+    pub fn sample(&self, t: f64) -> f64 { t * t * (self.end_speed - 2.0 * self.control_1 + self.start_speed) + 2.0 * t * (self.control_1 - self.start_speed) + self.start_speed }
+}
+
+pub(crate) struct QuinticHermite {
+    p0: (f64, f64),
+    p1: (f64, f64),
+    v0: (f64, f64),
+    v1: (f64, f64),
+    a0: (f64, f64),
+    a1: (f64, f64),
+}
+
+impl QuinticHermite {
+    pub fn sample(&self, t: f64) -> (f64, f64) {
+        (
+            (((((6.0 * self.p1.0 - 3.0 * self.v0.0 - 6.0 * self.p0.0 - 3.0 * self.v1.0 + 0.5 * self.a1.0 - 0.5 * self.a0.0) * t
+                + (-15.0 * self.p1.0 + 1.5 * self.a0.0 + 8.0 * self.v0.0 + 15.0 * self.p0.0 + 7.0 * self.v1.0 - self.a1.0))
+                * t
+                + (10.0 * self.p1.0 - 1.5 * self.a0.0 - 6.0 * self.v0.0 - 10.0 * self.p0.0 - 4.0 * self.v1.0 + 0.5 * self.a1.0))
+                * t
+                + (0.5 * self.a0.0))
+                * t
+                + self.v0.0)
+                * t
+                + self.p0.0,
+            (((((6.0 * self.p1.1 - 3.0 * self.v0.1 - 6.0 * self.p0.1 - 3.0 * self.v1.1 + 0.5 * self.a1.1 - 0.5 * self.a0.1) * t
+                + (-15.0 * self.p1.1 + 1.5 * self.a0.1 + 8.0 * self.v0.1 + 15.0 * self.p0.1 + 7.0 * self.v1.1 - self.a1.1))
+                * t
+                + (10.0 * self.p1.1 - 1.5 * self.a0.1 - 6.0 * self.v0.1 - 10.0 * self.p0.1 - 4.0 * self.v1.1 + 0.5 * self.a1.1))
+                * t
+                + (0.5 * self.a0.1))
+                * t
+                + self.v0.1)
+                * t
+                + self.p0.1,
+        )
+    }
+
+    pub fn sample_derivative(&self, t: f64) -> (f64, f64) {
+        (
+            (((((6.0 * self.p1.0 - 3.0 * self.v0.0 - 6.0 * self.p0.0 - 3.0 * self.v1.0 + 0.5 * self.a1.0 - 0.5 * self.a0.0) * t
+                + (-15.0 * self.p1.0 + 1.5 * self.a0.0 + 8.0 * self.v0.0 + 15.0 * self.p0.0 + 7.0 * self.v1.0 - self.a1.0))
+                * t
+                + (10.0 * self.p1.0 - 1.5 * self.a0.0 - 6.0 * self.v0.0 - 10.0 * self.p0.0 - 4.0 * self.v1.0 + 0.5 * self.a1.0))
+                * t
+                + (0.5 * self.a0.0))
+                * t
+                + self.v0.0)
+                * t
+                + self.p0.0,
+            (((((6.0 * self.p1.1 - 3.0 * self.v0.1 - 6.0 * self.p0.1 - 3.0 * self.v1.1 + 0.5 * self.a1.1 - 0.5 * self.a0.1) * t
+                + (-15.0 * self.p1.1 + 1.5 * self.a0.1 + 8.0 * self.v0.1 + 15.0 * self.p0.1 + 7.0 * self.v1.1 - self.a1.1))
+                * t
+                + (10.0 * self.p1.1 - 1.5 * self.a0.1 - 6.0 * self.v0.1 - 10.0 * self.p0.1 - 4.0 * self.v1.1 + 0.5 * self.a1.1))
+                * t
+                + (0.5 * self.a0.1))
+                * t
+                + self.v0.1)
+                * t
+                + self.p0.1,
+        )
+    }
+}
+
+pub(crate) struct PathSegment {
+    curve: QuinticHermite,
+    speed: SpeedCurve,
+    start_heading: f64,
+    end_heading: f64,
+    reversed_drive: bool,
+}
+
+pub(crate) enum Action {
+    ToggleMatchload,
+    SpinIntake,
+    StopIntake,
+    SpinIndexer,
+    StopIndexer,
+    SetPose(f64, f64, f64),
+}
+
+pub(crate) struct Auto {
+    spline: Vec<PathSegment>,
+    time_checkpoints: Vec<f64>,
+    spline_t: f64,
+    delay: f64,
+    actions: Vec<(Action, f64)>,
+    current_action: usize,
 }
